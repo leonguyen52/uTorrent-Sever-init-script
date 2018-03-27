e712a53d1e95bb2e2a118849343ab113bdf11e3f
Devilsking <leonguyen52@outlook.com>
2018-03-27 09:54:36 +0800

    Update utserver-init-script.sh

--- utserver-init-script.sh
+++ utserver-init-script.sh
@@ -50,7 +50,7 @@ FAILURE=false
 [ -r /etc/default/$NAME ] && . /etc/default/$NAME
 
 # Load the VERBOSE setting and other rcS variables
-. /lib/init/vars.sh
+#. /lib/init/vars.sh
 
 # Define LSB log_* functions.
 # Depend on lsb-base (>= 3.0-6) to ensure that this file is present.
